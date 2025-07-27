43eb5c3338be78fb15833d5a9da6eb12ec3a6935
xct258 <101554678+xct258@users.noreply.github.com>
2025-07-27 17:28:34 +0800

    优化上传备份脚本处理已经压制过的视频

--- 视频处理脚本/录播上传备份脚本.sh
+++ 视频处理脚本/录播上传备份脚本.sh
@@ -161,7 +161,7 @@ for dir in "${directories[@]}"; do
   fi
 
   # 获取录制平台
-  recording_platform=$(echo "$base_filename" | cut -d'_' -f 1)
+  recording_platform=$(echo "$base_filename" | cut -d'_' -f 1 | sed 's/^压制版-//')
   # 示例：录播姬
 
   backup_dir="${source_backup}/backup/${recording_platform}_${streamer_name}_${start_time}"
@@ -237,7 +237,7 @@ for backup_dir in "${sorted_backup_dirs[@]}"; do
   # 示例：暗区最穷
 
   # 获取录制平台
-  recording_platform=$(echo "$base_filename" | cut -d'_' -f 1)
+  recording_platform=$(echo "$base_filename" | cut -d'_' -f 1 | sed 's/^压制版-//')
   # 示例：录播姬
 
   # 获取主播名称
@@ -252,7 +252,7 @@ for backup_dir in "${sorted_backup_dirs[@]}"; do
   log info "主播名称: $streamer_name"
 
   # 投稿
-  if [[ "$streamer_name" == "括弧笑bilibili" && " ${update_servers[@]} " =~ " ${recording_platform} " ]]; then
+  if [[ "$streamer_name" == "括弧笑bilibili" && " ${update_servers[*]} " == *" $recording_platform "* ]]; then
     log info "开始投稿准备"
     # 安装xz工具
     if ! command -v xz &> /dev/null; then
@@ -317,73 +317,79 @@ for backup_dir in "${sorted_backup_dirs[@]}"; do
       filename_no_ext="${filename%.*}"
       # 示例：录播姬_2024年12月01日22点13分_暗区最穷_高机动持盾军官
 
-      if [[ "$streamer_name" == "括弧笑bilibili" && " ${update_servers[@]} " =~ " ${recording_platform} " ]]; then
+      if [[ "$streamer_name" == "括弧笑bilibili" && " ${update_servers[*]} " == *" $recording_platform "* ]]; then
         if [[ "$filename" == *.mp4 ]]; then
-          xml_file="${filename_no_ext}.xml"
-          ass_file="${filename_no_ext}.ass"
-          output_file="压制版-${filename_no_ext}.mp4"
-          if [[ -f "${backup_dir}/${xml_file}" ]]; then
-            # 检查 XML 是否包含有效弹幕（通过匹配<d，<sc，<gift，<guard开头的行）
-            if grep -aEq '^\s*<(d|sc|gift|guard)' "${backup_dir}/${xml_file}"; then
-              log info "检测到有效弹幕文件，开始弹幕压制：${backup_dir}"
+          if [[ "$filename" == 压制版-* ]]; then
+            log info "检测到压制版视频，跳过弹幕压制"
+            compressed_files+=("${backup_dir}/${filename}")
+            original_files+=("${backup_dir}/${filename}")
+          else
+            xml_file="${filename_no_ext}.xml"
+            ass_file="${filename_no_ext}.ass"
+            output_file="压制版-${filename_no_ext}.mp4"
+            if [[ -f "${backup_dir}/${xml_file}" ]]; then
+              # 检查 XML 是否包含有效弹幕（通过匹配<d，<sc，<gift，<guard开头的行）
+              if grep -aEq '^\s*<(d|sc|gift|guard)' "${backup_dir}/${xml_file}"; then
+                log info "检测到有效弹幕文件，开始弹幕压制：${backup_dir}"
 
-              #biliup_cover_image=$(python3 /rec/脚本/封面获取.py "$backup_dir")
-              #log debug "获取封面图片路径：$biliup_cover_image"
+                #biliup_cover_image=$(python3 /rec/脚本/封面获取.py "$backup_dir")
+                #log debug "获取封面图片路径：$biliup_cover_image"
 
-              # 检查 Intel 显卡驱动安装
-              if lspci | grep -i "VGA\|Display" | grep -i "Intel Corporation" > /dev/null; then
-                if ! vainfo > /dev/null 2>&1; then
-                  log info "检测到Intel显卡驱动未安装，开始安装..."
-                  apt update
-                  apt install -y gpg wget
-                  wget -qO - https://repositories.intel.com/gpu/intel-graphics.key | gpg --dearmor --output /usr/share/keyrings/intel-graphics.gpg
-                  echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/gpu/ubuntu jammy client" | tee /etc/apt/sources.list.d/intel-gpu-jammy.list
-                  apt update
-                  apt install -y intel-media-va-driver-non-free libmfx1 libmfxgen1 libvpl2 va-driver-all vainfo
-                  log success "Intel显卡驱动安装完成"
-                fi
-              fi
-
-              for lib in "${!libraries[@]}"; do
-                if ! python3 -c "import $lib" &> /dev/null; then
-                  if apt install -y "${libraries[$lib]}"; then
-                    log success "安装Python库 ${libraries[$lib]} 成功"
-                  else
-                    log error "安装Python库 ${libraries[$lib]} 失败"
-                    upload_success=false
+                # 检查 Intel 显卡驱动安装
+                if lspci | grep -i "VGA\|Display" | grep -i "Intel Corporation" > /dev/null; then
+                  if ! vainfo > /dev/null 2>&1; then
+                    log info "检测到Intel显卡驱动未安装，开始安装..."
+                    apt update
+                    apt install -y gpg wget
+                    wget -qO - https://repositories.intel.com/gpu/intel-graphics.key | gpg --dearmor --output /usr/share/keyrings/intel-graphics.gpg
+                    echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/gpu/ubuntu jammy client" | tee /etc/apt/sources.list.d/intel-gpu-jammy.list
+                    apt update
+                    apt install -y intel-media-va-driver-non-free libmfx1 libmfxgen1 libvpl2 va-driver-all vainfo
+                    log success "Intel显卡驱动安装完成"
                   fi
                 fi
-              done
-              if [[ "$ENABLE_DANMAKU_OVERLAY" != "true" ]]; then
-                log warn "已禁用高能进度条叠加，跳过视频压制"
-                compressed_files+=("${backup_dir}/${filename}")  # 直接添加原视频路径
-              else
-                /rec/apps/DanmakuFactory -i "${backup_dir}/${xml_file}" -o "${backup_dir}/${ass_file}" -S 50 -O 230 --ignore-warnings > /dev/null
-                if python3 /rec/脚本/压制视频.py "${backup_dir}/${xml_file}"; then
-                  if [[ -f "${backup_dir}/${output_file}" ]]; then
-                    log success "视频弹幕压制完成：$output_file"
-                    compressed_files+=("${backup_dir}/${output_file}")
+
+                for lib in "${!libraries[@]}"; do
+                  if ! python3 -c "import $lib" &> /dev/null; then
+                    if apt install -y "${libraries[$lib]}"; then
+                      log success "安装Python库 ${libraries[$lib]} 成功"
+                    else
+                      log error "安装Python库 ${libraries[$lib]} 失败"
+                      upload_success=false
+                    fi
+                  fi
+                done
+                if [[ "$ENABLE_DANMAKU_OVERLAY" != "true" ]]; then
+                  log warn "已禁用高能进度条叠加，跳过视频压制"
+                  compressed_files+=("${backup_dir}/${filename}")  # 直接添加原视频路径
+                else
+                  /rec/apps/DanmakuFactory -i "${backup_dir}/${xml_file}" -o "${backup_dir}/${ass_file}" -S 50 -O 230 --ignore-warnings > /dev/null
+                  if python3 /rec/脚本/压制视频.py "${backup_dir}/${xml_file}"; then
+                    if [[ -f "${backup_dir}/${output_file}" ]]; then
+                      log success "视频弹幕压制完成：$output_file"
+                      compressed_files+=("${backup_dir}/${output_file}")
+                    else
+                      log error "压制脚本执行成功但未生成目标文件，使用原视频：$filename"
+                      compressed_files+=("${backup_dir}/${filename}")
+                    fi
                   else
-                    log error "压制脚本执行成功但未生成目标文件，使用原视频：$filename"
+                    log error "视频弹幕压制失败：$output_file"
                     compressed_files+=("${backup_dir}/${filename}")
                   fi
-                else
-                  log error "视频弹幕压制失败：$output_file"
-                  compressed_files+=("${backup_dir}/${filename}")
+                  rm "${backup_dir}/${ass_file}"
+                  original_files+=("${backup_dir}/${filename}")
                 fi
-                rm "${backup_dir}/${ass_file}"
-                original_files+=("${backup_dir}/${filename}")
+              else
+                log warn "弹幕文件内容为空或不符合预期，跳过弹幕压制：${backup_dir}/${xml_file}"
               fi
             else
-              log warn "弹幕文件内容为空或不符合预期，跳过弹幕压制：${backup_dir}/${xml_file}"
+              log warn "未检测到弹幕 XML 文件，跳过弹幕压制：${backup_dir}/${xml_file}"
+              # 添加视频到数组
+              compressed_files+=("${backup_dir}/${filename}")
             fi
-          else
-            log warn "未检测到弹幕 XML 文件，跳过弹幕压制：${backup_dir}/${xml_file}"
-            # 添加视频到数组
-            compressed_files+=("${backup_dir}/${filename}")
+            # 同时将原始视频文件添加到原始文件数组
+            original_files+=("${backup_dir}/${filename}")
           fi
-          # 同时将原始视频文件添加到原始文件数组
-          original_files+=("${backup_dir}/${filename}")
         fi
       fi
     else
@@ -391,7 +397,7 @@ for backup_dir in "${sorted_backup_dirs[@]}"; do
     fi
   done
 
-  if [[ "$streamer_name" == "括弧笑bilibili" && " ${update_servers[@]} " =~ " ${recording_platform} " ]]; then
+  if [[ "$streamer_name" == "括弧笑bilibili" && " ${update_servers[*]} " == *" $recording_platform "* ]]; then
     # 构建视频标题
     upload_title_1="括弧笑${formatted_start_time_4}直播回放"
     # 构建视频简介
