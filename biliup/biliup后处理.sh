9e4602a7a3891f8ec4967afea5275490ebcd2276
xct258 <101554678+xct258@users.noreply.github.com>
2025-07-27 10:29:00 +0800

    修改biliup后处理脚本获取开播时间异常的bug

--- biliup/biliup后处理.sh
+++ biliup/biliup后处理.sh
@@ -13,7 +13,7 @@ for input_file in "${input_files[@]}"; do
   # 获取开播时间
   broadcast_start_time=$(echo "$base_filename" | cut -d '_' -f 2 | cut -d '.' -f 1)
   # 处理开播时间格式
-  formatted_start_time_4=$(echo "$broadcast_start_time" | sed -E 's/([0-9]{4})年([0-9]{2})月([0-9]{2})日([0-9]{2})点([0-9]{2})分/\1-\2-\3')
+  formatted_start_time_4=$(echo "$broadcast_start_time" | sed -E 's/([0-9]{4})年([0-9]{2})月([0-9]{2})日([0-9]{2})点([0-9]{2})分/\1-\2-\3/')
   # 示例：2025-03-26
   # 获取主播名称
   streamer_name=$(echo "$base_filename" | sed -E 's/.*_(.*)\..*/\1/')
